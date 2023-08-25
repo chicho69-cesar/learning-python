23e1da86c5640ded6395aaec1f21b64cfb48715a
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-25 16:58:30 -0600

    :art: Add hello world and data types

---
+++ basics/00_helloworld.py
@@ -0,0 +1,25 @@
+### Hello World ###
+
+print("Hello World")
+print('Hello World')
+
+# Esto es un comentario
+
+"""
+Este es un 
+comentario
+en varias lineas
+"""
+
+'''
+Este también es un
+comentario
+en varias lineas
+'''
+
+# Como consultar el tipo de dato
+print(type("Soy un tipo de dato string")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(3.1416)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType'

d8e6267f5deaef4586a86b92177afa4fc08cca99
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-29 21:11:13 -0600

    :sparkles: Add conditionals

---
+++ basics/08_conditionals.py
@@ -0,0 +1,31 @@
+### Conditionals ###
+
+# if
+my_condition = True
+
+if my_condition: # Es lo mismo que if my_condition == True:
+    print("Se ejecuta la condición del if")
+
+my_condition = 5 * 5
+
+if my_condition == 10:
+    print("Se ejecuta la condición del segundo if")
+
+# if, elif, else
+if my_condition > 10  and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
+elif my_condition == 25:
+    print("Es 25")
+else:
+    print("Es menor o igual que 10 o mayor o igual que 20 o distinto de 25")
+
+print("La ejecución continua")
+
+# Condicional con inspección de valor
+my_string = ""
+
+if not my_string:
+    print("Mi cadena de texto está vacía")
+
+if my_string == "Mi cadena de texto":
+    print("Estas cadenas de texto coinciden")

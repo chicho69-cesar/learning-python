9ab96bfe151885ffddc3fca2d52a64b6db03c67b
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-01-03 18:00:57 -0600

    :sparkles: Add package manager topic

---
+++ intermediate/mypackage/arithmetics.py
@@ -0,0 +1,25 @@
+### Arithmetics ###
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def mod(a, b):
+    return a % b
+
+def pot(a, b):
+    return a ** b
+
+def log(a, b):
+    return a // b
+
+def raiz(a, b):
+    return a ** (1 / b)
